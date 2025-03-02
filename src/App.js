acefef714e2d20e3193f40f4e59e60a0b842d97b
Robert Armstrong <rarmstro@gmail.com>
2025-03-02 14:39:34 -0500

    Added BlueprintJS. Downgraded to react version 18 because 19 isn't supported by blueprintjs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-import { React } from 'react';
+import React from "react";
 
-export function App() {
-  return <h1>Retirement Planner</h1>;
+import "@blueprintjs/core/lib/css/blueprint.css";
+import { Menu, Classes, MenuItem, MenuDivider, Icon, Navbar, NavbarGroup, NavbarHeading, NavbarDivider } from "@blueprintjs/core";
+
+function App() {
+  return (
+    <div>
+      <Navbar>
+        <NavbarGroup>
+          <NavbarHeading>Retirement Planner</NavbarHeading>
+          <NavbarDivider />
+          <MenuItem icon="home" text="Home" />
+          <MenuItem icon="new-link" text="WebLinks" />
+          <MenuItem icon="user" text="Profile" />
+          <MenuItem icon="cog" text="Setting" />
+        </NavbarGroup>
+      </Navbar>
+
+      <h4>ReactJS Blueprint Menu Component</h4>
+      <Menu className={Classes.ELEVATION_1}>
+        <MenuItem icon={<Icon icon="home" />} text="Home" />
+        <MenuDivider />
+        <MenuItem icon="new-link" text="WebLinks" />
+        <MenuItem icon="user" text="Profile" />
+        <MenuDivider />
+        <MenuItem icon="cog" text="Setting" />
+      </Menu>
+
+    </div>
+  );
 }
+
+export default App;
